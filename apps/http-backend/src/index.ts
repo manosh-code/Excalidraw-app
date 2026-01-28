d1f0516931787408bcd591a19782e8c3ced70d02
manosh-code <manoshsarkar098@yahoo.com>
2026-01-28 23:38:40 +0530

    http-backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,9 +1,31 @@
 import express from "express";
+import { JWT_SECRET } from "./config";
+import jwt from "jsonwebtoken";
+import { middleware } from "./middleware";
 
 const app = express();
 
 
+app.post("/signup", (req, res) => {
+
+})
+
+app.post("/signup", (req, res) => {
+    const userId = 1;
+    const token =jwt.sign({
+        userId
+    }, JWT_SECRET);
+
+    res.json({ token });
+
+    // Handle signup logic
+})
+
+app.post("/room" , middleware, async(req, res) => {
+    
+    // Handle room creation logic
+})
 
 app.listen(3001, () =>{
-    console.log("Server is running on port 3000");
+    console.log("Server is running on port 3001");
 })
